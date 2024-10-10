617f00d8d04f3633e978c212af69370bb7990e59
jjmow <dagantegan.ms09@nctu.edu.tw>
2024-10-10 12:54:13 +0800

    fix trainTestDataShuffle

--- trainTestDataShuffle.py
+++ trainTestDataShuffle.py
@@ -3,38 +3,50 @@ import shutil
 import random
 
 # Define source and destination directories
-sourceDir = '"D:/leveling/leveling_data/v1/Normal/source"'
+sourceDir = 'D:/leveling/leveling_data/v1/Normal/source'
 testDir = 'D:/leveling/leveling_data/v1/Normal/test'
 trainDir = 'D:/leveling/leveling_data/v1/Normal/train'
 
-testFileNum = 40
 
-# Function to clear a directory
-def clear_directory(directory):
-    for file in os.listdir(directory):
-        file_path = os.path.join(directory, file)
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)  # Remove file or symbolic link
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)  # Remove directory
+def cut(sourceDir, testDir, trainDir):
+    testFileNum = 40
 
-# Clear destination directories
-clear_directory(testDir)
-clear_directory(trainDir)
+    # Function to clear a directory
+    def clear_directory(directory):
+        for file in os.listdir(directory):
+            file_path = os.path.join(directory, file)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)  # Remove file or symbolic link
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)  # Remove directory
 
-# Get all file names from the source directory
-files = os.listdir(source_dir)
+    # Clear destination directories
+    clear_directory(testDir)
+    clear_directory(trainDir)
 
-# Randomly select 50 files to move to directory A
-selected_files = random.sample(files, testFileNum)
+    # Get all file names from the source directory
+    files = os.listdir(sourceDir)
 
-# Move selected files to directory A
-for file in selected_files:
-    shutil.copy(os.path.join(source_dir, file), os.path.join(testDir, file))
+    # Randomly select 50 files to move to directory A
+    selected_files = random.sample(files, testFileNum)
 
-# Move the remaining files to directory B
-remaining_files = [file for file in files if file not in selected_files]
-for file in remaining_files:
-    shutil.copy(os.path.join(source_dir, file), os.path.join(trainDir, file))
+    # Move selected files to directory A
+    for file in selected_files:
+        shutil.copy(os.path.join(sourceDir, file), os.path.join(testDir, file))
 
-print("Files have been cleared and moved successfully.")
+    # Move the remaining files to directory B
+    remaining_files = [file for file in files if file not in selected_files]
+    for file in remaining_files:
+        shutil.copy(os.path.join(sourceDir, file), os.path.join(trainDir, file))
+
+    print("Files have been cleared and moved successfully.")
+
+sourceDir = 'D:/leveling/leveling_data/v1/Normal/source'
+testDir = 'D:/leveling/leveling_data/v1/Normal/test'
+trainDir = 'D:/leveling/leveling_data/v1/Normal/train'
+cut(sourceDir, testDir, trainDir)
+
+sourceDir = 'D:/leveling/leveling_data/v1/Abnormal/source'
+testDir = 'D:/leveling/leveling_data/v1/Abnormal/test'
+trainDir = 'D:/leveling/leveling_data/v1/Abnormal/train'
+cut(sourceDir, testDir, trainDir)
